3e93599cee87828dd76916d250dd3419504b0f44
Osama <osama.ali.lasheen@gmail.com>
2024-08-21 18:00:39 +0300

    Add POST and PUT Methods to API Helper and Implement Product Addition/Update Services
    This commit introduces new functionalities to the API helper in the Dart application, specifically the addition of POST and PUT methods. These methods are designed to facilitate the creation and updating of resources via HTTP requests. The POST method is implemented to send data to the server, while the PUT method is intended for updating existing resources. Both methods include handling for authentication tokens and return decoded JSON responses upon successful requests.
    
    Additionally, this commit includes the implementation of services for adding and updating products. The `AddProduct` and `UpdateProduct` classes are introduced, which utilize the newly added POST and PUT methods from the API helper to send product data to a mock API. These services handle the creation and updating of product entries, including details such as title, price, description, image URL, and category. They return `ProductModel` instances upon successful API responses, encapsulating the product data in a structured format for further use within the application.
    
    Furthermore, the main application file (`main.dart`) is updated to demonstrate the usage of the HTTP POST request for adding a product, showcasing how the new functionalities can be integrated into the application's workflow. This update includes a modification to the `FloatingActionButton`'s onPressed event to execute an asynchronous POST request to the fake store API, logging the response body to the console for demonstration purposes.

---
+++ lib/services/update_product.dart
@@ -0,0 +1,29 @@
+
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:store_app/helper/api.dart';
+import 'package:store_app/models/product_model.dart';
+
+class UpdatateProduct {
+  Future <ProductModel> updateProduct(
+      {required String title,
+      required String price,
+      required String description,
+      required String image,
+      required String category}) async {
+    Response response =
+        await API().post(url: 'https://fakestoreapi.com/products', body: {
+      'title': title,
+      'price': price,
+      'description': description,
+      'image': image,
+      'category': category
+    });
+    if (response.statusCode == 200) {
+      return ProductModel.fromJson(jsonDecode(response.body));
+    } else{
+      throw Exception('Request failed with status: ${response.statusCode} with body: ${jsonDecode(response.body)}');  
+    }
+  }
+}
