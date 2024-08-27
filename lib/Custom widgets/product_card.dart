0f7d2d09ba8f505353cf58ab79dd62ea36b10a74
Osama <osama.ali.lasheen@gmail.com>
2024-08-27 14:12:46 +0300

    Refactor product card widget for improved UI and shadow effects
    Enhanced the product card widget by increasing its size to 220x220, adding a shadow effect for depth, and wrapping its content in a Card widget to improve visual consistency. Adjusted the main axis alignment to 'end' for better content presentation and updated the product name text style for clarity.

--- lib/Custom widgets/product_card.dart
+++ lib/Custom widgets/product_card.dart
@@ -15,37 +15,49 @@ class _ProductCardState extends State<ProductCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 200,
-      width: 200,
-      decoration: const BoxDecoration(
-        color: Colors.white,
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Image.asset(
-                'lib/assets/eldennnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn.png'),
-            const Align(
-                alignment: AlignmentDirectional.centerStart,
-                child: Text('Product name')),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text('Price'),
-                IconButton(
-                  onPressed: () {
-                    setState(() {
-                      isLiked = !isLiked;
-                    });
-                  },
-                  icon: Icon(isLiked ? Icons.favorite : Icons.favorite_border),
-                  color: isLiked ? Colors.red : Colors.grey,
-                )
-              ],
-            )
-          ],
+      height: 220,
+      width: 220,
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(0.3),
+          spreadRadius: 0,
+          blurRadius: 25,
+          offset: const Offset(10, 10), // changes position of shadow
+        ),
+      ]),
+      child: Card(
+        elevation: 5,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Product name',
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(r'$Price'),
+                  IconButton(
+                    onPressed: () {
+                      setState(() {
+                        isLiked = !isLiked;
+                      });
+                    },
+                    icon:
+                        Icon(isLiked ? Icons.favorite : Icons.favorite_border),
+                    color: isLiked ? Colors.red : Colors.grey,
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
